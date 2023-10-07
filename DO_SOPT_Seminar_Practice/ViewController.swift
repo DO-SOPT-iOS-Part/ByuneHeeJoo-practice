267c86ab0de6d299d9726c3b2bfb0d1506e59432
heejoo <heejoo_byun@naver.com>
2023-10-07 14:55:38 +0900

    Initial Commit

---
+++ DO_SOPT_Seminar_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DO_SOPT_Seminar_Practice
+//
+//  Created by 변희주 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
