342df1befa4bb00c4372b5e0bd3d10cbbad0c3d7
heejoo <heejoo_byun@naver.com>
2023-10-13 11:31:23 +0900

    [Feat] 인스턴스 수정 및 생성

--- DO_SOPT_Seminar_Practice/ViewController.swift
+++ DO_SOPT_Seminar_Practice/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var idTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var loginCheckButton: UIButton!
+    
     private var idText: String = ""
     private var passwordText: String = ""
     @IBOutlet var descriptionLabel: UILabel!
@@ -16,6 +21,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setStyle()
+    }
+    
+    func setStyle() {
+        setDescriptionLabel()
+        setIdTextField()
+        setPasswordTextField()
+        setLoginCheckButton()
+    }
+    
+    func setDescriptionLabel() {
+        descriptionLabel.font = .systemFont(ofSize: 33)
+    }
+    
+    func setIdTextField() {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: idTextField.frame.size.height + 3, width: idTextField.frame.width, height: 1)
+        border.backgroundColor = UIColor.systemGray3.cgColor
+        idTextField.layer.addSublayer((border))
+        idTextField.textAlignment = .left
+        idTextField.textColor = UIColor.black
+        idTextField.placeholder = "카카오메일 아이디, 이메일, 전화번호"
+        idTextField.font = .systemFont(ofSize: 17)
+
+    }
+    
+    func setPasswordTextField() {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: passwordTextField.frame.size.height + 3, width: passwordTextField.frame.width, height: 1)
+        border.backgroundColor = UIColor.systemGray3.cgColor
+        passwordTextField.layer.addSublayer((border))
+        passwordTextField.textAlignment = .left
+        passwordTextField.textColor = UIColor.black
+        passwordTextField.placeholder = "비밀번호"
+        passwordTextField.font = .systemFont(ofSize: 17)
+
+    }
+    
+    func setLoginCheckButton() {
+        loginCheckButton.backgroundColor = .yellow
+        loginCheckButton.layer.cornerRadius = 11
+        loginCheckButton.setImage(UIImage(named: "checkMark"), for: .normal)
     }
     
     @IBAction func loginButtonTap(_ sender: Any) {
