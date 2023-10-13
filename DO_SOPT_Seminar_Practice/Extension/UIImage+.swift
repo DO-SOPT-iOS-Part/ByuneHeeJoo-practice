e17ae122a61e3128fb300d984ed681eb422ee7d9
heejoo <heejoo_byun@naver.com>
2023-10-13 11:32:05 +0900

    [Chore] Extension 파일 추가

---
+++ DO_SOPT_Seminar_Practice/Extension/UIImage+.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+.swift
+//  DO_SOPT_Seminar_Practice
+//
+//  Created by 변희주 on 2023/10/13.
+//
+
+import UIKit
+
+extension UIImage {
+    func resize(targetSize: CGSize) -> UIImage? {
+        let newRect = CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newRect.size, true, 0)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        context.interpolationQuality = .high
+        draw(in: newRect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+}
