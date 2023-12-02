a4d8a7f97e85baf604425b35d313ca3e743397a4
heejoo <heejoo_byun@naver.com>
2023-12-02 15:14:50 +0900

    Feat [#10] 7주차 세팅

---
+++ DO_SOPT_Seminar_Practice/Presentation/SeventhSeminar/SoptMember.swift
@@ -0,0 +1,13 @@
+//
+//  SoptMember.swift
+//  DO_SOPT_Seminar_Practice
+//
+//  Created by 변희주 on 2023/12/02.
+//
+
+import Foundation
+
+struct SoptMember {
+    let name: String
+    var age: Int
+}
